a17f395feb603de78fb28e9b52f1268885f125f0
Nick Hallstrom <nhallstrom@proofpoint.com>
2020-04-07 22:13:40 -0600

    Better messages for unimplemented events

--- src/main/scala/discord/model/DispatchEvent.scala
+++ src/main/scala/discord/model/DispatchEvent.scala
@@ -26,81 +26,81 @@ object DispatchEvent {
 
   implicit val readyDecoder: Decoder[Ready] = deriveConfiguredDecoder
 
-  val ImplementMe = DecodingFailure("UNIMPLEMENTED", Nil).asLeft
+  def ImplementMe(name: String) = DecodingFailure(s"UNIMPLEMENTED: $name", Nil).asLeft
 
   def decodeEventName(eventName: String, data: ACursor): Decoder.Result[DispatchEvent] = eventName match {
     case "READY" =>
       data.as[Ready]
     case "RESUMED" =>
       Resumed.asRight
-    case "CHANNEL_CREATE" =>
-      ImplementMe
-    case "CHANNEL_UPDATE" =>
-      ImplementMe
-    case "CHANNEL_DELETE" =>
-      ImplementMe
-    case "CHANNEL_PINS_UPDATE" =>
-      ImplementMe
+    case n @ "CHANNEL_CREATE" =>
+      ImplementMe(n)
+    case n @ "CHANNEL_UPDATE" =>
+      ImplementMe(n)
+    case n @ "CHANNEL_DELETE" =>
+      ImplementMe(n)
+    case n @ "CHANNEL_PINS_UPDATE" =>
+      ImplementMe(n)
     case "GUILD_CREATE" =>
       data.as[Json].map(GuildCreate)
-    case "GUILD_UPDATE" =>
-      ImplementMe
-    case "GUILD_DELETE" =>
-      ImplementMe
-    case "GUILD_BAN_ADD" =>
-      ImplementMe
-    case "GUILD_BAN_REMOVE" =>
-      ImplementMe
-    case "GUILD_EMOJIS_UPDATE" =>
-      ImplementMe
-    case "GUILD_INTEGRATIONS_UPDATE" =>
-      ImplementMe
-    case "GUILD_MEMBER_ADD" =>
-      ImplementMe
-    case "GUILD_MEMBER_REMOVE" =>
-      ImplementMe
+    case n @ "GUILD_UPDATE" =>
+      ImplementMe(n)
+    case n @ "GUILD_DELETE" =>
+      ImplementMe(n)
+    case n @ "GUILD_BAN_ADD" =>
+      ImplementMe(n)
+    case n @ "GUILD_BAN_REMOVE" =>
+      ImplementMe(n)
+    case n @ "GUILD_EMOJIS_UPDATE" =>
+      ImplementMe(n)
+    case n @ "GUILD_INTEGRATIONS_UPDATE" =>
+      ImplementMe(n)
+    case n @ "GUILD_MEMBER_ADD" =>
+      ImplementMe(n)
+    case n @ "GUILD_MEMBER_REMOVE" =>
+      ImplementMe(n)
     case "GUILD_MEMBER_UPDATE" =>
       data.as[Json].map(GuildMemberUpdate)
-    case "GUILD_MEMBERS_CHUNK" =>
-      ImplementMe
-    case "GUILD_ROLE_CREATE" =>
-      ImplementMe
-    case "GUILD_ROLE_UPDATE" =>
-      ImplementMe
-    case "GUILD_ROLE_DELETE" =>
-      ImplementMe
-    case "INVITE_CREATE" =>
-      ImplementMe
-    case "INVITE_DELETE" =>
-      ImplementMe
+    case n @ "GUILD_MEMBERS_CHUNK" =>
+      ImplementMe(n)
+    case n @ "GUILD_ROLE_CREATE" =>
+      ImplementMe(n)
+    case n @ "GUILD_ROLE_UPDATE" =>
+      ImplementMe(n)
+    case n @ "GUILD_ROLE_DELETE" =>
+      ImplementMe(n)
+    case n @ "INVITE_CREATE" =>
+      ImplementMe(n)
+    case n @ "INVITE_DELETE" =>
+      ImplementMe(n)
     case "MESSAGE_CREATE" =>
       data.as[Message].map(MessageCreate)
-    case "MESSAGE_UPDATE" =>
-      ImplementMe
-    case "MESSAGE_DELETE" =>
-      ImplementMe
-    case "MESSAGE_DELETE_BULK" =>
-      ImplementMe
+    case n @ "MESSAGE_UPDATE" =>
+      ImplementMe(n)
+    case n @ "MESSAGE_DELETE" =>
+      ImplementMe(n)
+    case n @ "MESSAGE_DELETE_BULK" =>
+      ImplementMe(n)
     case "MESSAGE_REACTION_ADD" =>
       data.as[Json].map(ReactionAdd)
-    case "MESSAGE_REACTION_REMOVE" =>
-      ImplementMe
-    case "MESSAGE_REACTION_REMOVE_ALL" =>
-      ImplementMe
-    case "MESSAGE_REACTION_REMOVE_EMOJI" =>
-      ImplementMe
+    case n @ "MESSAGE_REACTION_REMOVE" =>
+      ImplementMe(n)
+    case n @ "MESSAGE_REACTION_REMOVE_ALL" =>
+      ImplementMe(n)
+    case n @ "MESSAGE_REACTION_REMOVE_EMOJI" =>
+      ImplementMe(n)
     case "PRESENCE_UPDATE" =>
       data.as[Json].map(PresenceUpdate)
     case "TYPING_START" =>
       data.as[Json].map(TypingStart)
-    case "USER_UPDATE" =>
-      ImplementMe
-    case "VOICE_STATE_UPDATE" =>
-      ImplementMe
-    case "VOICE_SERVER_UPDATE" =>
-      ImplementMe
-    case "WEBHOOKS_UPDATE" =>
-      ImplementMe
+    case n @ "USER_UPDATE" =>
+      ImplementMe(n)
+    case n @ "VOICE_STATE_UPDATE" =>
+      ImplementMe(n)
+    case n @ "VOICE_SERVER_UPDATE" =>
+      ImplementMe(n)
+    case n @ "WEBHOOKS_UPDATE" =>
+      ImplementMe(n)
     case unknown =>
       DecodingFailure(s"Unknown event name received: $unknown", data.history).asLeft
   }
