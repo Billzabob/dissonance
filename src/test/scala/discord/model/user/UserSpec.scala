598836f49d3918d442124120f35ea8be34daa9d5
hogiyogi597 <stevyhogan@gmail.com>
2020-04-29 17:51:56 -0600

    Moved test objects to resources folder and added test util to read them out. Probably need to improve the TestUtils since I am just doing unsafeRunSync but oh well; it is a good start.

--- src/test/scala/discord/model/user/UserSpec.scala
+++ src/test/scala/discord/model/user/UserSpec.scala
@@ -7,21 +7,15 @@ import discord.model.user.PremiumType.NitroClassic
 import discord.model.user.UserRole.HouseBravery
 import cats.implicits._
 import discord.model.DiscordId
+import discord.TestUtils._
+import cats.effect.ContextShift
+import scala.concurrent.ExecutionContext
+import cats.effect.IO
 
 class UserSpec extends AnyFlatSpec with Matchers {
-    "a user json" should "be parsed correctly" in {
-        val rawJson = """{
-                            "id": "80351110224678912",
-                            "username": "Nelly",
-                            "discriminator": "1337",
-                            "avatar": "8342729096ea3675442027381ff50dfe",
-                            "verified": true,
-                            "email": "nelly@discordapp.com",
-                            "flags": 64,
-                            "premium_type": 1,
-                            "public_flags": 64
-                        }"""
-        val expectedUser = User(
+    implicit val contextShift: ContextShift[IO] = IO.contextShift(ExecutionContext.global)
+
+    val baseUser = User(
             id = DiscordId(80351110224678912L),
             username = "Nelly",
             discriminator = "1337",
@@ -37,40 +31,22 @@ class UserSpec extends AnyFlatSpec with Matchers {
             publicFlags = List(HouseBravery)
         )
 
+    "a user json" should "be parsed correctly" in {
+        val rawJson = readFileFromResource("/models/partialUser.json").unsafeRunSync()
+
+        val expectedUser = baseUser
+
         parse(rawJson).flatMap(_.as[User]) mustBe Right(expectedUser)
     }
 
     "a full user json" should "be parsed correctly" in {
-        val rawJson = """{
-                    "id": "80351110224678912",
-                    "username": "Nelly",
-                    "discriminator": "1337",
-                    "avatar": "8342729096ea3675442027381ff50dfe",
-                    "bot": false,
-                    "system": false,
-                    "mfa_enabled": true,
-                    "locale": "en",
-                    "verified": true,
-                    "email": "nelly@discordapp.com",
-                    "flags": 64,
-                    "premium_type": 1,
-                    "public_flags": 64
-                }"""
+        val rawJson = readFileFromResource("/models/fullUser.json").unsafeRunSync()
 
-        val expectedUser = User(
-            id = DiscordId(80351110224678912L),
-            username = "Nelly",
-            discriminator = "1337",
-            avatar = "8342729096ea3675442027381ff50dfe".some,
+        val expectedUser = baseUser.copy(
             bot = false.some,
             system = false.some,
             mfaEnabled = true.some,
-            locale = "en".some,
-            verified = true.some,
-            email = "nelly@discordapp.com".some,
-            flags = List(HouseBravery),
-            premiumType = NitroClassic.some,
-            publicFlags = List(HouseBravery)
+            locale = "en".some
         )
 
         parse(rawJson).flatMap(_.as[User]) mustBe Right(expectedUser)
