2b45243ad7bf096b821590bcea49c4b79adf3c13
Nick Hallstrom <nhallstrom@proofpoint.com>
2020-05-04 19:39:38 -0600

    Finished all event decoders. Probably still need a lot of changes after testing

--- src/main/scala/dissonance/model/Event.scala
+++ src/main/scala/dissonance/model/Event.scala
@@ -12,24 +12,23 @@ import io.circe.generic.extras.Configuration
 import io.circe.generic.extras.semiauto._
 import java.time.OffsetDateTime
 import java.time.Instant
+import org.http4s.circe._
+import org.http4s.Uri
 
 sealed trait Event extends Product with Serializable
 
 object Event {
-  case class ChannelCreate(channel: Channel) extends Event
-  case class ChannelDelete(channel: Channel) extends Event
-  case class ChannelPinsUpdate(
-      guildId: Snowflake,
-      channelId: Snowflake,
-      lastPinTimestamp: OffsetDateTime
-  ) extends Event
-  case class ChannelUpdate(channel: Channel)                          extends Event
-  case class GuildBanAdd(guildBan: guild.Ban)                         extends Event
-  case class GuildBanRemove(guildBan: guild.Ban)                      extends Event
-  case class GuildCreate(guild: Guild)                                extends Event
-  case class GuildEmojis(guildId: Snowflake, emojis: List[Emoji])     extends Event
-  case class GuildId(guildId: Snowflake)                              extends Event
-  case class GuildMemberAdd(guildId: Snowflake, member: guild.Member) extends Event
+  case class ChannelCreate(channel: Channel)                                                               extends Event
+  case class ChannelDelete(channel: Channel)                                                               extends Event
+  case class ChannelPinsUpdate(guildId: Snowflake, channelId: Snowflake, lastPinTimestamp: OffsetDateTime) extends Event
+  case class ChannelUpdate(channel: Channel)                                                               extends Event
+  case class GuildBanAdd(guildBan: guild.Ban)                                                              extends Event
+  case class GuildBanRemove(guildBan: guild.Ban)                                                           extends Event
+  case class GuildCreate(guild: Guild)                                                                     extends Event
+  case class GuildDelete(guild: Guild)                                                                     extends Event
+  case class GuildEmojis(guildId: Snowflake, emojis: List[Emoji])                                          extends Event
+  case class GuildId(guildId: Snowflake)                                                                   extends Event
+  case class GuildMemberAdd(guildId: Snowflake, member: guild.Member)                                      extends Event
   case class GuildMembersChunk(
       guildId: Snowflake,
       members: List[guild.Member],
@@ -39,18 +38,12 @@ object Event {
       presences: Option[List[Presence]],
       nonce: Option[String]
   ) extends Event
-  case class GuildMemberRemove(guildId: Snowflake, user: User) extends Event
-  case class GuildMemberUpdate(
-      guildId: Snowflake,
-      roles: List[Snowflake],
-      user: User,
-      nick: Option[String],
-      premiumSince: Option[OffsetDateTime]
-  ) extends Event
-  case class GuildRoleCreate(guildId: Snowflake, role: guild.Role)  extends Event
-  case class GuildRoleDelete(guildId: Snowflake, roleId: Snowflake) extends Event
-  case class GuildRoleUpdate(guildId: Snowflake, role: guild.Role)  extends Event
-  case class GuildUpdate(guild: Guild)                              extends Event
+  case class GuildMemberRemove(guildId: Snowflake, user: User)                                                                                     extends Event
+  case class GuildMemberUpdate(guildId: Snowflake, roles: List[Snowflake], user: User, nick: Option[String], premiumSince: Option[OffsetDateTime]) extends Event
+  case class GuildRoleCreate(guildId: Snowflake, role: guild.Role)                                                                                 extends Event
+  case class GuildRoleDelete(guildId: Snowflake, roleId: Snowflake)                                                                                extends Event
+  case class GuildRoleUpdate(guildId: Snowflake, role: guild.Role)                                                                                 extends Event
+  case class GuildUpdate(guild: Guild)                                                                                                             extends Event
   case class InviteCreate(
       channelId: Snowflake,
       code: String,
@@ -64,55 +57,47 @@ object Event {
       temporary: Boolean,
       uses: Int
   ) extends Event
-  case class InviteDelete(channelId: Snowflake, guildId: Option[Snowflake], code: String)              extends Event
-  case class MessageCreate(message: Message)                                                           extends Event
-  case class MessageDelete(id: Snowflake, channelId: Snowflake, guildId: Option[Snowflake])            extends Event
-  case class MessageDeleteBulk(ids: List[Snowflake], channelId: Snowflake, guildId: Option[Snowflake]) extends Event
-  case class MessageReactionAdd(
-      userId: Snowflake,
-      channelId: Snowflake,
-      messageId: Snowflake,
-      guildId: Option[Snowflake],
-      member: Option[guild.Member],
-      emoji: Emoji
-  ) extends Event
-  case class MessageUpdate(message: Message)    extends Event // TODO: Probably won't work since inner message for Update might only have id and channelId.
-  case class PresenceUpdate(presence: Presence) extends Event
-  case class Ready(
-      v: Integer,
-      user: User,
-      sessionId: String,
-      shard: Option[(Int, Int)] // TODO: Parse the 2 element array into a case class instead of tuple
-  ) extends Event
-  case object Resumed extends Event
-  case class TypingStart(
-      channelId: Snowflake,
-      guildId: Option[Snowflake],
-      userId: Snowflake,
-      timestamp: Instant,
-      member: Option[guild.Member]
-  ) extends Event
-
-  def ImplementMe(name: String) = DecodingFailure(s"UNIMPLEMENTED: $name", Nil).asLeft
+  case class InviteDelete(channelId: Snowflake, guildId: Option[Snowflake], code: String)                                                                              extends Event
+  case class MessageCreate(message: Message)                                                                                                                           extends Event
+  case class MessageDelete(id: Snowflake, channelId: Snowflake, guildId: Option[Snowflake])                                                                            extends Event
+  case class MessageDeleteBulk(ids: List[Snowflake], channelId: Snowflake, guildId: Option[Snowflake])                                                                 extends Event
+  case class MessageReactionAdd(userId: Snowflake, channelId: Snowflake, messageId: Snowflake, guildId: Option[Snowflake], member: Option[guild.Member], emoji: Emoji) extends Event
+  case class MessageReactionRemove(userId: Snowflake, channelId: Snowflake, messageId: Snowflake, guildId: Option[Snowflake], emoji: Emoji)                            extends Event
+  case class MessageReactionRemoveAll(channelId: Snowflake, messageId: Snowflake, guildId: Option[Snowflake])                                                          extends Event
+  case class MessageReactionRemoveEmoji(channelId: Snowflake, messageId: Snowflake, guildId: Option[Snowflake], emoji: Emoji)                                          extends Event
+  case class MessageUpdate(message: Message)                                                                                                                           extends Event // TODO: Probably won't work since inner message for Update might only have id and channelId.
+  case class PresenceUpdate(presence: Presence)                                                                                                                        extends Event
+  case class Ready(v: Integer, user: User, sessionId: String, shard: Option[(Int, Int)])                                                                               extends Event // TODO: Parse the 2 element array into a case class instead of tuple
+  case object Resumed                                                                                                                                                  extends Event
+  case class TypingStart(channelId: Snowflake, guildId: Option[Snowflake], userId: Snowflake, timestamp: Instant, member: Option[guild.Member])                        extends Event
+  case class UserUpdate(user: User)                                                                                                                                    extends Event
+  case class VoiceStateUpdate(voiceState: guild.VoiceState)                                                                                                            extends Event
+  case class VoiceServerUpdate(token: String, guildId: Snowflake, endpoint: Uri)                                                                                       extends Event
+  case class WebhookUpdate(guildId: Snowflake, channelId: Snowflake)                                                                                                   extends Event
 
   implicit val config: Configuration = Configuration.default.withSnakeCaseMemberNames
 
-  implicit val channelPinsUpdateDecoder: Decoder[ChannelPinsUpdate] = deriveConfiguredDecoder
-  implicit val guildEmojisDecoder: Decoder[GuildEmojis]             = deriveConfiguredDecoder
-  implicit val guildIdDecoder: Decoder[GuildId]                     = deriveConfiguredDecoder
-  implicit val guildMemberRemoveDecoder: Decoder[GuildMemberRemove] = deriveConfiguredDecoder
-  implicit val guildMembersChunkDecoder: Decoder[GuildMembersChunk] = deriveConfiguredDecoder
-  implicit val guildMemberUpdateDecoder: Decoder[GuildMemberUpdate] = deriveConfiguredDecoder
-  implicit val guildRoleCreateDecoder: Decoder[GuildRoleCreate]     = deriveConfiguredDecoder
-  implicit val guildRoleDeleteDecoder: Decoder[GuildRoleDelete]     = deriveConfiguredDecoder
-  implicit val guildRoleUpdateDecoder: Decoder[GuildRoleUpdate]     = deriveConfiguredDecoder
-  implicit val inviteCreateDecoder: Decoder[InviteCreate]           = deriveConfiguredDecoder
-  implicit val inviteDeleteDecoder: Decoder[InviteDelete]           = deriveConfiguredDecoder
-  implicit val messageDelete: Decoder[MessageDelete]                = deriveConfiguredDecoder
-  implicit val messageDeleteBulk: Decoder[MessageDeleteBulk]        = deriveConfiguredDecoder
-  implicit val messageReactionAdd: Decoder[MessageReactionAdd]      = deriveConfiguredDecoder
-  implicit val readyDecoder: Decoder[Ready]                         = deriveConfiguredDecoder
-  implicit val typingStartDecoder: Decoder[TypingStart]             = deriveConfiguredDecoder
+  implicit val channelPinsUpdateDecoder: Decoder[ChannelPinsUpdate]            = deriveConfiguredDecoder
+  implicit val guildEmojisDecoder: Decoder[GuildEmojis]                        = deriveConfiguredDecoder
+  implicit val guildIdDecoder: Decoder[GuildId]                                = deriveConfiguredDecoder
+  implicit val guildMemberRemoveDecoder: Decoder[GuildMemberRemove]            = deriveConfiguredDecoder
+  implicit val guildMembersChunkDecoder: Decoder[GuildMembersChunk]            = deriveConfiguredDecoder
+  implicit val guildMemberUpdateDecoder: Decoder[GuildMemberUpdate]            = deriveConfiguredDecoder
+  implicit val guildRoleCreateDecoder: Decoder[GuildRoleCreate]                = deriveConfiguredDecoder
+  implicit val guildRoleDeleteDecoder: Decoder[GuildRoleDelete]                = deriveConfiguredDecoder
+  implicit val guildRoleUpdateDecoder: Decoder[GuildRoleUpdate]                = deriveConfiguredDecoder
+  implicit val inviteCreateDecoder: Decoder[InviteCreate]                      = deriveConfiguredDecoder
+  implicit val inviteDeleteDecoder: Decoder[InviteDelete]                      = deriveConfiguredDecoder
+  implicit val messageDelete: Decoder[MessageDelete]                           = deriveConfiguredDecoder
+  implicit val messageDeleteBulk: Decoder[MessageDeleteBulk]                   = deriveConfiguredDecoder
+  implicit val messageReactionAdd: Decoder[MessageReactionAdd]                 = deriveConfiguredDecoder
+  implicit val messageReactionRemove: Decoder[MessageReactionRemove]           = deriveConfiguredDecoder
+  implicit val messageReactionRemoveAll: Decoder[MessageReactionRemoveAll]     = deriveConfiguredDecoder
+  implicit val messageReactionRemoveEmoji: Decoder[MessageReactionRemoveEmoji] = deriveConfiguredDecoder
+  implicit val readyDecoder: Decoder[Ready]                                    = deriveConfiguredDecoder
+  implicit val typingStartDecoder: Decoder[TypingStart]                        = deriveConfiguredDecoder
+  implicit val voiceServerUpdateDecoder: Decoder[VoiceServerUpdate]            = deriveConfiguredDecoder
+  implicit val webhookUpdateDecoder: Decoder[WebhookUpdate]                    = deriveConfiguredDecoder
 
   // TODO: Finish implementing all the events:
   // https://discordapp.com/developers/docs/topics/gateway#commands-and-events-gateway-events
@@ -133,9 +118,8 @@ object Event {
       data.as[Guild].map(GuildCreate)
     case "GUILD_UPDATE" =>
       data.as[Guild].map(GuildUpdate)
-    case n @ "GUILD_DELETE" =>
-      // Not sure on this one, since the docs give an example but no schema
-      ImplementMe(n)
+    case "GUILD_DELETE" =>
+      data.as[Guild].map(GuildDelete)
     case "GUILD_BAN_ADD" =>
       data.as[guild.Ban].map(GuildBanAdd)
     case "GUILD_BAN_REMOVE" =>
@@ -172,24 +156,24 @@ object Event {
       data.as[MessageDeleteBulk]
     case "MESSAGE_REACTION_ADD" =>
       data.as[MessageReactionAdd]
-    case n @ "MESSAGE_REACTION_REMOVE" =>
-      ImplementMe(n)
-    case n @ "MESSAGE_REACTION_REMOVE_ALL" =>
-      ImplementMe(n)
-    case n @ "MESSAGE_REACTION_REMOVE_EMOJI" =>
-      ImplementMe(n)
+    case "MESSAGE_REACTION_REMOVE" =>
+      data.as[MessageReactionRemove]
+    case "MESSAGE_REACTION_REMOVE_ALL" =>
+      data.as[MessageReactionRemoveAll]
+    case "MESSAGE_REACTION_REMOVE_EMOJI" =>
+      data.as[MessageReactionRemoveEmoji]
     case "PRESENCE_UPDATE" =>
       data.as[Presence].map(PresenceUpdate)
     case "TYPING_START" =>
       data.as[TypingStart]
-    case n @ "USER_UPDATE" =>
-      ImplementMe(n)
-    case n @ "VOICE_STATE_UPDATE" =>
-      ImplementMe(n)
-    case n @ "VOICE_SERVER_UPDATE" =>
-      ImplementMe(n)
-    case n @ "WEBHOOKS_UPDATE" =>
-      ImplementMe(n)
+    case "USER_UPDATE" =>
+      data.as[User].map(UserUpdate)
+    case "VOICE_STATE_UPDATE" =>
+      data.as[guild.VoiceState].map(VoiceStateUpdate)
+    case "VOICE_SERVER_UPDATE" =>
+      data.as[VoiceServerUpdate]
+    case "WEBHOOKS_UPDATE" =>
+      data.as[WebhookUpdate]
     case unknown =>
       DecodingFailure(s"Unknown event name received: $unknown", data.history).asLeft
   }
