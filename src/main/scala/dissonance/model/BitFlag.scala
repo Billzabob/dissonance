e835249d4272fd933ec46960e3183be4c36a2fee
Nick Hallstrom <nhallstrom@proofpoint.com>
2020-05-05 15:24:00 -0600

    Small refactor

--- src/main/scala/dissonance/model/BitFlag.scala
+++ src/main/scala/dissonance/model/BitFlag.scala
@@ -4,11 +4,11 @@ import cats.implicits._
 import enumeratum.{Enum, EnumEntry}
 import io.circe.Decoder
 
-trait BitFlag { val mask: Int }
+trait BitFlag extends EnumEntry { val mask: Int }
 
 object BitFlag {
 
-  def decoder[A <: BitFlag with EnumEntry](enum: Enum[A]): Decoder[List[A]] =
+  def decoder[A <: BitFlag](enum: Enum[A]): Decoder[List[A]] =
     Decoder[Option[Int]].map(_.map(flags => enum.values.toList.filter(passesBitMask(flags))).orEmpty)
 
   private def passesBitMask(flags: Int)(bitFlag: BitFlag) =
