7f8451214ba4eb1aa2a475fd942284bd542f6147
Nick Hallstrom <nhallstrom@proofpoint.com>
2020-05-06 22:41:10 -0600

    Minor changes suggested by Steph

--- src/main/scala/dissonance/model/package.scala
+++ src/main/scala/dissonance/model/package.scala
@@ -7,7 +7,7 @@ package object model {
   // TODO: Implement
   type Snowflake = Long
   // TODO: newtypes for all kinds of Snowflakes
-  @newtype case class DiscordId(value: Snowflake) // TODO: Change to Snowflake
+  @newtype case class DiscordId(value: Snowflake)
 
   object DiscordId {
     implicit val idDecoder: Decoder[DiscordId] = Decoder[Long].map(DiscordId.apply)
--- src/test/scala/dissonance/TestUtils.scala
+++ src/test/scala/dissonance/TestUtils.scala
@@ -6,7 +6,7 @@ import cats.effect.ContextShift
 
 object TestUtils {
   // TODO: Return Stream[IO, String] of lines from file
-  def readFileFromResource(path: String)(implicit contextShift: ContextShift[IO]) = {
+  def readFileFromResource(path: String)(implicit contextShift: ContextShift[IO]): IO[List[String]] = {
     val acquire = IO.shift *> IO(Source.fromURL(getClass.getResource(path)))
 
     acquire.bracket { in =>
