403eaca25402a3c6e4c6e221f299dab1856a414b
Scala Steward <me@scala-steward.org>
2020-11-26 04:21:06 +0100

    Update http4s-circe to 0.21.13

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -6,7 +6,7 @@ object Dependencies {
     val fs2             = "2.4.6"
     val cats            = "2.2.0"
     val circe           = "0.13.0"
-    val http4s          = "0.21.12"
+    val http4s          = "0.21.13"
     val newtype         = "0.4.4"
     val refined         = "0.9.18"
     val scalaTest       = "3.2.3"
