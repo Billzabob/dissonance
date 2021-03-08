7228b470ee551a60011b04bffbaa424619dc6ca2
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2021-03-08 10:18:32 +0100

    Update scalatest to 3.2.6 (#153)

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -9,7 +9,7 @@ object Dependencies {
     val http4s          = "0.21.20"
     val newtype         = "0.4.4"
     val refined         = "0.9.21"
-    val scalaTest       = "3.2.4"
+    val scalaTest       = "3.2.6"
     val catsEffect      = "2.3.3"
     val enumeratum      = "1.6.1"
     val websocketClient = "0.3.5"
