f9f24b291eb010db919ec63f6640bb67aee2804f
Scala Steward <me@scala-steward.org>
2021-03-29 18:30:44 +0200

    Update fs2-core to 3.0.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -3,7 +3,7 @@ import sbt._
 object Dependencies {
 
   object Versions {
-    val fs2             = "2.5.3"
+    val fs2             = "3.0.0"
     val cats            = "2.4.2"
     val circe           = "0.13.0"
     val http4s          = "0.21.21"
