1d717b583796304f3b5d9e20d4e841620e0716a7
Scala Steward <me@scala-steward.org>
2021-04-21 18:11:02 +0200

    Update scalatest to 3.2.8

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -9,7 +9,7 @@ object Dependencies {
     val http4s          = "0.21.22"
     val newtype         = "0.4.4"
     val refined         = "0.9.24"
-    val scalaTest       = "3.2.7"
+    val scalaTest       = "3.2.8"
     val catsEffect      = "2.5.0"
     val enumeratum      = "1.6.1"
     val websocketClient = "0.3.6"
