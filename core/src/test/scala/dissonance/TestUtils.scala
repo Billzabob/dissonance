656e6aff92de7df82ffc80ae379621d36c506fa0
hogiyogi597 <stevyhogan@gmail.com>
2021-04-22 19:08:10 -0600

    Added in local loading of DissonanceIT bot token in a .secret file in the test/resources.

--- core/src/test/scala/dissonance/TestUtils.scala
+++ core/src/test/scala/dissonance/TestUtils.scala
@@ -6,9 +6,12 @@ import cats.effect.IO
 import fs2.io.file.Files
 
 object TestUtils {
+  def getResourcePath(path: String): Path =
+    Path.of(getClass.getResource(path).toURI)
+
   def readFileFromResource(path: String): fs2.Stream[IO, String] = {
     Files[IO]
-      .readAll(Path.of(getClass.getResource(path).toURI), 4096)
+      .readAll(getResourcePath(path), 4096)
       .through(fs2.text.utf8Decode)
       .through(fs2.text.lines)
       .dropLastIf(_.isEmpty)
