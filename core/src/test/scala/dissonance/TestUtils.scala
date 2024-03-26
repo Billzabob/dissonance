ab8236a2f9fcaf6bfeffa0966d6fbfc0b0128065
Scala Steward <scala_steward@virtuslab.com>
2024-03-26 14:53:34 +0000

    Applied Scalafix rule(s) replace:fs2.text.utf8Decode/fs2.text.utf8.decode, replace:fs2.text.utf8DecodeC/fs2.text.utf8.decodeC, replace:fs2.text.utf8Encode/fs2.text.utf8.encode, replace:fs2.text.utf8EncodeC/fs2.text.utf8.encodeC

--- core/src/test/scala/dissonance/TestUtils.scala
+++ core/src/test/scala/dissonance/TestUtils.scala
@@ -4,12 +4,13 @@ import java.nio.file.Path
 
 import cats.effect.IO
 import fs2.io.file.Files
+import fs2.text
 
 object TestUtils {
   def readFileFromResource(path: String): fs2.Stream[IO, String] = {
     Files[IO]
       .readAll(Path.of(getClass.getResource(path).toURI), 4096)
-      .through(fs2.text.utf8Decode)
+      .through(text.utf8.decode)
       .through(fs2.text.lines)
       .dropLastIf(_.isEmpty)
   }
